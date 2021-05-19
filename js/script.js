d23f82cc5a72849dff63cd7112c377c4f6a90d56
Роман <camomile4@yandex.ru>
2021-05-19 19:28:50 +0300

    Создан файл test.js в папке testjs и файл test.html с подключением скрипта

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// let number = +prompt("Введите число");
+
+// if(!number && typeof number== "object")
+//  {alert("Вы отменили");}
+// else {
+//     if(number = "");
+//     {alert ("Вы ничего не ввели")}
+// }
+// let flag = confirm ("Сегодня тепло?")
+// if(flag) {
+//     alert ("Вы ошибаетесь");
+// }
+// else { alert ("Вы правы");
+// }
+
+
+pars
+
+// let mas = [];
+// mas.push(10,200);
+// mas.push(20);
+let Date = new Date;
+let hourNow = today;
+let greeting
+
+if (hourNow>18) {greeting="Good evening";
+}
+else  if (hourNow>12) {greeting="Good afternoon"   
+}
+else{greeting="Добро пожаловать";}
+    
+document.write(greeting);
+
+
+// for(let i=0; num=0, i<10; num++) {
+
+//     if(num % 2 == 0) {
+// mas[i]=num;
+// i++;
+//     }
+//     // else {
+//     //     // continue;
+//     // }
+// }
+
+// for(let i = 0 ; i< mas.length; i++)
+// {
+//     console.log(`${i+1}. ${mas[i]}`);
+// }
